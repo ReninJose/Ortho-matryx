f57a3a931788fa8742bbef431b69e2f1734525ee
Ryan White <ryanw5758@gmail.com>
2022-06-08 12:49:42 -0400

    Add main menu traversal, matrix representation
    Rev 1.3: functionality to return to main menu, matrix representation with Canvas widget on gameLoop screen, handling for destruction/creation of matrix

--- capstone_guidev/ddrgui.py
+++ capstone_guidev/ddrgui.py
@@ -1,13 +1,12 @@
-# Main UI REV 1.2
-# 1.2 Changes: framework for avatar selection/gameloop screen
+# Main UI REV 1.3
+# 1.3 Changes: functionality to return to main menu, matrix representation with Canvas widget on gameLoop screen
+# refactor into OOP approach: more readable and easier to edit frames between functions
 
-# TODO Next Revision: add GUI class (with args/kwargs), add representation for light matrix in gameLoop, add functionality to return to MM after avatar screen/gameLoop
-
-# Needs to: run on Raspberry Pi power on (exec UNIX command), take button input from several different I/O pins (? depends on how we implement), access and discriminate between each pin,
-# change color depending on what button was pressed
+# TODO Next Revision: displaying avatars on avatar select screen (store path in variable most likely)
+# refactor into OOP approach: more readable and easier to edit frames between functions
 
 import tkinter as tk
-from tkinter import Frame
+from tkinter import Canvas, Frame
 
 # create root and frames globally to ensure functions can access data
 root = tk.Tk()
@@ -24,10 +23,37 @@ def LaunchAvatar():
 def LaunchQP():
     print("Entered Quick Play")
     gameScreen.pack(fill='both', expand=1)
+    LoadMatrix()
     mainMenu.pack_forget()
     avatarSelect.pack_forget()
 
-# TODO: add mainMenu frame function to enter in the gameScreen/avatarSelect frames
+def LaunchMM():
+    print("Entered mainMenu")
+    mainMenu.pack(fill='both', expand=1)
+    avatarSelect.pack_forget()
+
+    # delete only the Canvas widget: deleting the rest blows up the whole GUI since frame is only initialized once
+    for widget in gameScreen.winfo_children():
+        if (type(widget) == Canvas):
+            widget.destroy()
+            break
+
+    gameScreen.pack_forget()
+
+def LoadMatrix():
+    xIter = 0; yIter = 0
+    matrix = tk.Canvas(gameScreen, bg="white", height=250, width=300)
+
+    # probably not efficient: refactor in next revision
+    # doesn't matter what color the squares are for now: data from the MCU will decide that in a later revision
+    for i in range(9):
+        matrix.create_rectangle(xIter, yIter, xIter+20, yIter+20, fill='red')
+        if xIter == 40:
+            yIter += 20
+            xIter = 0
+        else:
+            xIter += 20
+    matrix.pack()
 
 def main():
 
@@ -35,10 +61,15 @@ def main():
     root.geometry('400x400') # in final version, needs to be fixed to size of monitor, borderless, etc.
 
     # initialize frame displays
-    labelQP = tk.Label(gameScreen, text="Displaying game screen", font="Helvetica")
-    labelQP.pack()
+    labelGS = tk.Label(gameScreen, text="Displaying game screen", font="Helvetica")
+    labelGS.pack()
     labelAvatar = tk.Label(avatarSelect, text="Displaying avatar select", font="Helvetica")
     labelAvatar.pack()
+
+    returnMM_GS = tk.Button(gameScreen, text = "Return to Main Menu", command = lambda:LaunchMM())
+    returnMM_GS.pack()
+    returnMM_AS = tk.Button(avatarSelect, text = "Return to Main Menu", command = lambda:LaunchMM())
+    returnMM_AS.pack()
 
     # title: thinking about making something in GIMP or photoshop instead of just a plain label
     titleVar = tk.StringVar()
