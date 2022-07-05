3e44c8a4bbc3825a3a2c610b601ddf6dd120ad09
ReninJose <reninking@gmail.com>
2022-07-05 08:58:48 -0700

    Pre-finalized score-board

--- backend/backend.cpp
+++ backend/backend.cpp
@@ -3,25 +3,35 @@
 #include<iostream>
 #include<stdlib.h>
 #include<unistd.h>
-#include<pthread.h>
+//#include<pthread.h>
+#include<string>
 
 using namespace std;
 
-const char* test_id = "john";
-const char* test_score = "9";
+int main(int argc, char* argv[]){
 
-void* score_board(void*) {      
-    execlp("./score_board/sb", "sb", test_id, test_score , NULL);
-    return NULL;
-}
+    const char* id;
+    const char* score;
+    string command = argv[1];
 
-int main(){
+    if (argc != 4) {
+        cout << "Invalid # of argument count" << endl;
+        return 1;
+    }
+    // Initiate code for scoreboard
+    if (command.compare("sb") == 0) {
+        id = argv[2];
+        cout << id;
+        score = argv[3];
+        cout << " " << score << endl;
+        cout << "Process overlaying sb_generator.cpp over backend.cpp ..." << endl;
+        execlp("./score_board/sb", "sb", id, score , NULL);     // Calling execlp ends this program here
+    }
+    // Initiate code for Color Generator
+    else if (command.compare("cg") == 0) {
+        // open color generator code & and most likely keep the backend thread running.
+    }
 
-    pthread_t call_sb;
-    
-    // Calling sb_generator.cpp
-    pthread_create(&call_sb, NULL, score_board, NULL);
-    pthread_join(call_sb,NULL);
 
     return 0;
 }
