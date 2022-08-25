0a65ee6597965b1296d707543e04210abe8de5bc
ReninJose <reninking@gmail.com>
2022-08-25 06:23:05 -0700

    Added new support to backend.cpp. Added Initiator for piClient.py

--- backend/backend.cpp
+++ backend/backend.cpp
@@ -1,11 +1,10 @@
 // Author: Renin Kingsly Jose
-// Rev 2.0
+// Rev 2.5
 
 #include<iostream>
 #include<string>
 #include<stdlib.h>
 #include<unistd.h>
-//#include<pthread.h>
 #include<sys/wait.h>
 
 using namespace std;
@@ -14,20 +13,31 @@ int main(int argc, char* argv[]){
 
     pid_t pid;              // For process ID 
     int status;
-    const char* id;
-    const char* score;
     string command = argv[1];
 
-    if (argc != 4) {
-        cout << "Invalid # of argument count" << endl;
-        return 1;
-    }
     // Initiate code for scoreboard
     if (command.compare("sb") == 0) {
+        
+        if (argc != 4) {
+            cout << "Invalid # of argument count" << endl;
+            return 1;
+        }
+
+        const char* id;
+        const char* score;
+
         id = argv[2];
         cout << id;
         score = argv[3];
         cout << " " << score << endl;
+
+        if(execl("/home/renin/Documents/Ortho-matryx/backend/score_board/sb", "sb", id, score , NULL) < 0) {
+            perror("Execl failed");
+            return 1;
+        }    
+    }
+    // Initiate code for Color Generator
+    else if (command.compare("cg") == 0) {
         pid = fork();                   // Forking new process
 
         if(pid < 0) {
@@ -37,22 +47,25 @@ int main(int argc, char* argv[]){
 
         // Child
         if(pid == 0) {
-            if(execl("/home/renin/Documents/Ortho-matryx/backend/score_board/sb", "sb", id, score , NULL) < 0) {
+            if(execl("/home/renin/Documents/Ortho-matryx/backend/color_rand_gen/color", "color", NULL) < 0) {
                 perror("Execl failed");
                 return 1;
-            }    
+            }
         }
         // Parent
-        else {
-            // Parent waits until child process ends
+        else if(pid > 0) {
+            // Wait for child process to finish
             wait(&status);
-        }
-    }
-    // Initiate code for Color Generator
-    else if (command.compare("cg") == 0) {
-        // open color generator code & and most likely keep the backend thread running.
-    }
 
-
+            // This section executes piClient.py
+            if (execl("/home/renin/Documents/Ortho-matryx/pi-client/Initiator", "Initiator", NULL) < 0) {            // FINISH THIS
+                perror("Execl failed");
+                return 1;
+            }
+        }    
+    }
     return 0;
 }
+
+
+    
